39a9790379fcd20ada8e4e16b43f6cfbefa536b1
estherjsuh <estherjsuh@gmail.com>
2021-01-06 10:32:23 -0800

    Initial commit

---
+++ backend/routes/exercises.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+let User = require('../models/exercise.model');
+
+
+//GET Request
+//find() - mongoose method; returns a promise
+router.route('/').get((req, res) => {
+    Exercise.find()
+        .then(exercises => res.json(exercises))
+        .catch(err => res.status(400).json('Error: '+ err));
+});
+
+//POST Request
+//save() - mongoose method
+router.route('/add').post((req, res) => {
+    const username = req.body.username;
+    const description = req.body.description;
+    const duration = Number(req.body.duration);
+    const date = Date.parse(req.body.date);
+
+    const newExercise = new Exercise({
+        username,
+        description,
+        duration,
+        date
+    });
+
+    newExercise.save()
+    .then(() => res.json('Exercise added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+
+module.exports = router;
